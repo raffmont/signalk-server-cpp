d502b88b4d7d75a2826c39b242ac4d984aa7f7ca
Raffaele Montella <raffaele.montella@uniparthenope.it>
2018-09-12 15:43:36 +0200

    Added replace substring in string private method

--- core/providers/NMEA0183DataProvider.hpp
+++ core/providers/NMEA0183DataProvider.hpp
@@ -6,11 +6,21 @@
 #define SIGNALK_SERVER_CPP_NMEA0183DATAPROVIDER_HPP
 
 
+
 #include "DataProvider.hpp"
+
+#include <marnav/nmea/angle.hpp>
+#include <marnav/nmea/nmea.hpp>
+#include <marnav/nmea/zda.hpp>
+#include <marnav/nmea/gll.hpp>
+#include <marnav/utils/unique.hpp>
 
 class NMEA0183DataProvider: public DataProvider {
 public:
     void parse(std::string line);
+
+private:
+    bool replace(std::string& str, const std::string& from, const std::string& to);
 };
 
 
