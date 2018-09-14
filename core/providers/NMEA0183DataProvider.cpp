d988446d2f25eb7f48e327a20c0480c693c0ba5f
Raffaele Montella <raffaele.montella@uniparthenope.it>
2018-09-14 16:00:32 +0200

    Added new nmea to signalk parsers

--- core/providers/NMEA0183DataProvider.cpp
+++ core/providers/NMEA0183DataProvider.cpp
@@ -30,37 +30,190 @@ void NMEA0183DataProvider::parse(std::string line) {
 
                 } else if (sentence->id() == nmea::sentence_id::GLL) {
                     auto gll = nmea::sentence_cast<nmea::gll>(sentence.get());
-                    //if (gll->get_data_valid()) {
-                    utils::optional<geo::longitude> longitude = gll->get_longitude();
-                    utils::optional<geo::latitude> latitude = gll->get_latitude();
+                    utils::optional<nmea::status> status=gll->get_data_valid();
+                    if (status.available()) {
+                        utils::optional<geo::longitude> longitude = gll->get_longitude();
+                        utils::optional<geo::latitude> latitude = gll->get_latitude();
 
-                    json update = makeUpdate(nmea::to_string(sentence->get_talker()), nmea::to_string(sentence->id()));
-                    update["values"] = {
+                        json updatePosition = makeUpdate(nmea::to_string(sentence->get_talker()), nmea::to_string(sentence->id()));
+                        updatePosition["values"] = {
+                                {
+                                        {"path", "navigation.position"},
+                                        {"value", {
+                                                          {"longitude", longitude.value().get()},
+                                                          {"latitude", latitude.value().get()}
+                                                  }
+                                        }
+                                }
+                        };
+                        updates.push_back(updatePosition);
+                    }
+                } else if (sentence->id() == nmea::sentence_id::RMC) {
+                    auto rmc = nmea::sentence_cast<nmea::rmc>(sentence.get());
+                    utils::optional<char> status=rmc->get_status();
+                    if (status.available()) {
+                        utils::optional<geo::longitude> longitude = rmc->get_longitude();
+                        utils::optional<geo::latitude> latitude = rmc->get_latitude();
+
+                        utils::optional<double> heading = rmc->get_heading();
+                        utils::optional<double> sog = rmc->get_sog();
+                        utils::optional<double> mag = rmc->get_mag();
+
+                        json updatePosition = makeUpdate(nmea::to_string(sentence->get_talker()),
+                                                         nmea::to_string(sentence->id()));
+                        updatePosition["values"] = {
+                                {
+                                        {"path", "navigation.position"},
+                                        {"value", {
+                                                          {"longitude", longitude.value().get()},
+                                                          {"latitude", latitude.value().get()}
+
+                                                  }
+                                        }
+                                }
+                        };
+                        updates.push_back(updatePosition);
+
+                        json updateMagneticVariation = makeUpdate(nmea::to_string(sentence->get_talker()),
+                                                                  nmea::to_string(sentence->id()));
+                        updateMagneticVariation["values"] = {
+                                {
+                                        {"path", "navigation.magneticVariation"},
+                                        {"value", deg2rad(mag.value())
+                                        }
+                                }
+                        };
+                        updates.push_back(updateMagneticVariation);
+
+                        json updateCourseOverGroundTrue = makeUpdate(nmea::to_string(sentence->get_talker()),
+                                                                     nmea::to_string(sentence->id()));
+                        updateCourseOverGroundTrue["values"] = {
+                                {
+                                        {"path", "navigation.courseOverGroundTrue"},
+                                        {"value", deg2rad(heading.value())
+                                        }
+                                }
+                        };
+                        updates.push_back(updateCourseOverGroundTrue);
+
+                        json courseOverGroundMagnetic = makeUpdate(nmea::to_string(sentence->get_talker()),
+                                                                   nmea::to_string(sentence->id()));
+                        updateCourseOverGroundTrue["values"] = {
+                                {
+                                        {"path", "navigation.courseOverGroundMagnetic"},
+                                        {"value", deg2rad(heading.value() + mag.value())
+                                        }
+                                }
+                        };
+                        updates.push_back(updateCourseOverGroundTrue);
+
+                        json updateSpeedOverGround = makeUpdate(nmea::to_string(sentence->get_talker()),
+                                                                nmea::to_string(sentence->id()));
+                        updateSpeedOverGround["values"] = {
+                                {
+                                        {"path", "navigation.speedOverGround"},
+                                        {"value", knots2ms(sog.value())
+                                        }
+                                }
+                        };
+                        updates.push_back(updateSpeedOverGround);
+                    }
+
+                } else if (sentence->id() == nmea::sentence_id::MWD) {
+                    auto mwd = nmea::sentence_cast<nmea::mwd>(sentence.get());
+
+                    utils::optional<double> directionMag=mwd->get_direction_mag();
+                    utils::optional<double> directionTrue=mwd->get_direction_true();
+                    utils::optional<double> speedKn=mwd->get_speed_kn();
+
+                    json updateDirectionMagnetic = makeUpdate(nmea::to_string(sentence->get_talker()), nmea::to_string(sentence->id()));
+                    updateDirectionMagnetic["values"] = {
+                        {
+                            {"path", "environment.wind.directionMagnetic"},
+                            {"value", deg2rad(directionMag.value())}
+                        }
+                    };
+                    updates.push_back(updateDirectionMagnetic);
+
+                    json updateDirectionTrue = makeUpdate(nmea::to_string(sentence->get_talker()), nmea::to_string(sentence->id()));
+                    updateDirectionTrue["values"] = {
                             {
-                                    {"path", "navigation.position"},
-                                    {"value", {
-                                                      {"longitude", longitude.value().get()},
-                                                      {"latitude", latitude.value().get()}
-                                              }
+                                    {"path", "environment.wind.directionTrue"},
+                                    {"value", deg2rad(directionTrue.value())}
+                            }
+                    };
+                    updates.push_back(updateDirectionTrue);
+
+                    json updateSpeedTrue = makeUpdate(nmea::to_string(sentence->get_talker()), nmea::to_string(sentence->id()));
+                    updateSpeedTrue["values"] = {
+                            {
+                                    {"path", "environment.wind.speedTrue"},
+                                    {"value", knots2ms(speedKn.value())}
+                            }
+                    };
+                    updates.push_back(updateSpeedTrue);
+
+                } else if (sentence->id() == nmea::sentence_id::VTG) {
+                    auto vtg = nmea::sentence_cast<nmea::vtg>(sentence.get());
+
+                    utils::optional<double> speedKn=vtg->get_speed_kn();
+                    utils::optional<double> trackMagn=vtg->get_track_magn();
+                    utils::optional<double> trackTrue=vtg->get_track_true();
+
+                    json updateCourseOverGroundTrue = makeUpdate(nmea::to_string(sentence->get_talker()),
+                                                                 nmea::to_string(sentence->id()));
+                    updateCourseOverGroundTrue["values"] = {
+                            {
+                                    {"path", "navigation.courseOverGroundTrue"},
+                                    {"value", deg2rad(trackTrue.value())
                                     }
                             }
                     };
-                    updates.push_back(update);
+                    updates.push_back(updateCourseOverGroundTrue);
 
+                    json courseOverGroundMagnetic = makeUpdate(nmea::to_string(sentence->get_talker()),
+                                                               nmea::to_string(sentence->id()));
+                    updateCourseOverGroundTrue["values"] = {
+                            {
+                                    {"path", "navigation.courseOverGroundMagnetic"},
+                                    {"value", deg2rad(trackMagn.value())
+                                    }
+                            }
+                    };
+                    updates.push_back(updateCourseOverGroundTrue);
 
-                    //std::cout << "Pos:" << nmea::to_string(longitude.value()) << "," << nmea::to_string(latitude.value())
-                    //          << "\n";
+                    json updateSpeedOverGround = makeUpdate(nmea::to_string(sentence->get_talker()),
+                                                            nmea::to_string(sentence->id()));
+                    updateSpeedOverGround["values"] = {
+                            {
+                                    {"path", "navigation.speedOverGround"},
+                                    {"value", knots2ms(speedKn.value())
+                                    }
+                            }
+                    };
 
-                    //}
-                } else if (sentence->id() == nmea::sentence_id::MWD) {
-
-                } else if (sentence->id() == nmea::sentence_id::VTG) {
+                    updates.push_back(updateSpeedOverGround);
 
                 } else if (sentence->id() == nmea::sentence_id::APB) {
 
                 } else if (sentence->id() == nmea::sentence_id::BWC) {
 
                 } else if (sentence->id() == nmea::sentence_id::DBT) {
+                    auto dbt = nmea::sentence_cast<nmea::dbt>(sentence.get());
+
+                    utils::optional<double > depthM = dbt->get_depth_meter();
+
+                    json updateBelowTransducer = makeUpdate(nmea::to_string(sentence->get_talker()),
+                                                            nmea::to_string(sentence->id()));
+                    updateBelowTransducer["values"] = {
+                            {
+                                    {"path", "environment.depth.belowTransducer"},
+                                    {"value", depthM.value()
+                                    }
+                            }
+                    };
+
+                    updates.push_back(updateBelowTransducer);
 
                 } else if (sentence->id() == nmea::sentence_id::GGA) {
                     auto gga = nmea::sentence_cast<nmea::gga>(sentence.get());
@@ -69,8 +222,8 @@ void NMEA0183DataProvider::parse(std::string line) {
                     utils::optional<geo::latitude> latitude = gga->get_latitude();
                     utils::optional<double> altitude = gga->get_altitude();
 
-                    json update = makeUpdate(nmea::to_string(sentence->get_talker()), nmea::to_string(sentence->id()));
-                    update["values"] = {
+                    json updatePosition = makeUpdate(nmea::to_string(sentence->get_talker()), nmea::to_string(sentence->id()));
+                    updatePosition["values"] = {
                             {
                                     {"path", "navigation.position"},
                                     {"value", {
@@ -81,17 +234,123 @@ void NMEA0183DataProvider::parse(std::string line) {
                                     }
                             }
                     };
-                    updates.push_back(update);
+                    updates.push_back(updatePosition);
 
                 } else if (sentence->id() == nmea::sentence_id::HDG) {
+                    auto hdg = nmea::sentence_cast<nmea::hdg>(sentence.get());
+
+                    utils::optional<double> magnVar=hdg->get_magn_var();
+                    utils::optional<nmea::direction> magnVarHem=hdg->get_magn_var_hem();
+
+                    if (magnVarHem.value()==nmea::direction::east) {
+                        magnVar=magnVar.value()*-1;
+                    }
+
+                    json updateMagneticVariation = makeUpdate(nmea::to_string(sentence->get_talker()),
+                                                            nmea::to_string(sentence->id()));
+                    updateMagneticVariation["values"] = {
+                            {
+                                    {"path", "navigation.magneticVariation"},
+                                    {"value", deg2rad(magnVar.value()) }
+                            }
+                    };
+
+                    updates.push_back(updateMagneticVariation);
 
                 } else if (sentence->id() == nmea::sentence_id::HDM) {
+                    auto hdm = nmea::sentence_cast<nmea::hdm>(sentence.get());
+
+                    utils::optional<double> headingMagn=hdm->get_heading();
+
+                    json updateHeadingMagnetic = makeUpdate(nmea::to_string(sentence->get_talker()),
+                                                            nmea::to_string(sentence->id()));
+                    updateHeadingMagnetic["values"] = {
+                        {
+                            {"path", "navigation.headingMagnetic"},
+                            {"value", deg2rad(headingMagn.value()) }
+                        }
+                    };
+
+                    updates.push_back(updateHeadingMagnetic);
 
                 } else if (sentence->id() == nmea::sentence_id::HDT) {
+                    auto hdt = nmea::sentence_cast<nmea::hdt>(sentence.get());
+
+                    utils::optional<double> headingTrue=hdt->get_heading();
+
+                    json updateHeadingTrue = makeUpdate(nmea::to_string(sentence->get_talker()),
+                                                            nmea::to_string(sentence->id()));
+                    updateHeadingTrue["values"] = {
+                            {
+                                    {"path", "navigation.headingTrue"},
+                                    {"value", deg2rad(headingTrue.value()) }
+                            }
+                    };
+
+                    updates.push_back(updateHeadingTrue);
 
                 } else if (sentence->id() == nmea::sentence_id::RSA) {
+                    auto rsa = nmea::sentence_cast<nmea::rsa>(sentence.get());
+
+                    utils::optional<nmea::status> status1=rsa->get_rudder1_valid();
+                    if (status1.available()) {
+
+                        utils::optional<double> rudder1=rsa->get_rudder1();
+
+                        json updateRudderAngle1 = makeUpdate(nmea::to_string(sentence->get_talker()),
+                                                           nmea::to_string(sentence->id()));
+                        updateRudderAngle1["values"] = {
+                                {
+                                        {"path", "steering.rudderAngle"},
+                                        {"value", deg2rad(rudder1.value()) }
+                                }
+                        };
+
+                        updates.push_back(updateRudderAngle1);
+                    }
+
 
                 } else if (sentence->id() == nmea::sentence_id::MWV) {
+                    auto mwv = nmea::sentence_cast<nmea::mwv>(sentence.get());
+
+                    utils::optional<double> angle = mwv->get_angle();
+                    utils::optional<nmea::reference> angleRef = mwv->get_angle_ref();
+                    utils::optional<double> speed = mwv->get_speed();
+                    utils::optional<nmea::unit::velocity> speedUnit = mwv->get_speed_unit();
+
+                    if (angleRef.value() == nmea::reference::RELATIVE) {
+
+                        json updateAngleApparent = makeUpdate(nmea::to_string(sentence->get_talker()),
+                                                              nmea::to_string(sentence->id()));
+
+                        updateAngleApparent["values"] = {
+                                {
+                                        {"path", "environment.wind.angleApparent"},
+                                        {"value", deg2rad(angle.value())}
+                                }
+                        };
+
+                        updates.push_back(updateAngleApparent);
+                    } else if (angleRef.value() == nmea::reference::TRUE) {
+
+                        json updateAngleTrueWater = makeUpdate(nmea::to_string(sentence->get_talker()),
+                                                              nmea::to_string(sentence->id()));
+
+                        updateAngleTrueWater["values"] = {
+                                {
+                                        {"path", "environment.wind.angleTrueWater"},
+                                        {"value", deg2rad(angle.value())}
+                                }
+                        };
+
+                        updates.push_back(updateAngleTrueWater);
+                    }
+
+                    if (speedUnit.value()==nmea::unit::velocity::knot) {
+                        speed=knots2ms(speed.value());
+                    } else if (speedUnit.value()==nmea::unit::velocity::kmh) {
+                        speed=kmh2ms(speed.value());
+                    }
 
                 } else if (sentence->id() == nmea::sentence_id::VHW) {
 
--- core/providers/NMEA0183DataProvider.hpp
+++ core/providers/NMEA0183DataProvider.hpp
@@ -14,6 +14,15 @@
 #include <marnav/nmea/zda.hpp>
 #include <marnav/nmea/gll.hpp>
 #include <marnav/nmea/gga.hpp>
+#include <marnav/nmea/rmc.hpp>
+#include <marnav/nmea/mwd.hpp>
+#include <marnav/nmea/vtg.hpp>
+#include <marnav/nmea/dbt.hpp>
+#include <marnav/nmea/hdg.hpp>
+#include <marnav/nmea/hdm.hpp>
+#include <marnav/nmea/hdt.hpp>
+#include <marnav/nmea/rsa.hpp>
+#include <marnav/nmea/mwv.hpp>
 #include <marnav/utils/unique.hpp>
 
 class NMEA0183DataProvider: public DataProvider {
@@ -24,6 +33,21 @@ private:
     bool replace(std::string& str, const std::string& from, const std::string& to);
     std::string currentISO8601TimeUTC();
     nlohmann::json makeUpdate(std::string talker, std::string id);
+    double PI=3.141592653589793238463L;
+    double D2R=0.0174533;
+    double r2d=57.2957795;
+    double KT2MS=0.514444;
+    double ms2kt=1.94384;
+    double KMH2MS=0.277777778;
+    double deg2rad(double deg) {
+        return deg * D2R;
+    }
+    double knots2ms(double knots) {
+        return knots*KT2MS;
+    }
+    double kmh2ms(double kmh) {
+        return kmh*KMH2MS;
+    }
 };
 
 
