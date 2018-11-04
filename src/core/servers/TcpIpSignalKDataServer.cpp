b1d66a6ee22be1426f8fb28663f148834d6a0cc2
Raffaele Montella <raffaele.montella@uniparthenope.it>
2018-11-04 18:24:56 +0100

    Implemented subscription. The hearthbeat is not working Handling close connection is not clean

--- src/core/servers/TcpIpSignalKDataServer.cpp
+++ src/core/servers/TcpIpSignalKDataServer.cpp
@@ -7,11 +7,9 @@
 
 #include "TcpIpSignalKDataServer.hpp"
 
-TcpIpSignalKDataServer::TcpIpSignalKDataServer(bool enabled,std::string id,SignalK::SignalKModel *pSignalKModel,  std::string bind, int port) {
-    this->enabled=enabled;
+TcpIpSignalKDataServer::TcpIpSignalKDataServer(bool enabled,std::string id,SignalK::SignalKModel *pSignalKModel,  std::string bind, int port) :SignalKDataServer(enabled,id,pSignalKModel){
     this->type="servers/signalk/tcpip";
-    this->id=id;
-    this->pSignalKModel=pSignalKModel;
+    this->bind=bind;
     this->port=port;
 }
 
@@ -23,26 +21,89 @@ TcpIpSignalKDataServer::~TcpIpSignalKDataServer() {
 
 
 void TcpIpSignalKDataServer::onRun(){
+    spdlog::get("console")->info("TcpIpSignalKDataServer/onRun: {0}:{1}",bind,port);
+
+    // Crate the loop
     auto loop = uvw::Loop::getDefault();
 
+    // Create the tcp handler
     std::shared_ptr<uvw::TcpHandle> tcp = loop->resource<uvw::TcpHandle>();
 
+    // Tcp on error event...
     tcp->on<uvw::ErrorEvent>([](const uvw::ErrorEvent &, uvw::TcpHandle &) {
         std::cout << "error " << std::endl;
     });
 
-    tcp->once<uvw::ListenEvent>([this](const uvw::ListenEvent &, uvw::TcpHandle &srv) {
-        std::cout << "listen" << std::endl;
+    // Tcp on listen event...
+    tcp->on<uvw::ListenEvent>([this](const uvw::ListenEvent &, uvw::TcpHandle &srv) {
+        spdlog::get("console")->info("onRun:tcp->once:ListenEvent/START");
 
+        // Create the client handler
         std::shared_ptr<uvw::TcpHandle> client = srv.loop().resource<uvw::TcpHandle>();
 
-        client->on<uvw::ErrorEvent>([](const uvw::ErrorEvent &, uvw::TcpHandle &) {
-            std::cout << "error " << std::endl;
+
+        // On error event...
+        client->on<uvw::ErrorEvent>([](const uvw::ErrorEvent &event, uvw::TcpHandle &handle) {
+            spdlog::get("console")->info("onRun:client->on:ErrorEvent --:",event.what());
         });
 
-        client->on<uvw::CloseEvent>([ptr = srv.shared_from_this()](const uvw::CloseEvent &, uvw::TcpHandle &) {
-            std::cout << "close" << std::endl;
+
+        // On close event...
+        client->on<uvw::CloseEvent>([ptr = srv.shared_from_this()](const uvw::CloseEvent &, uvw::TcpHandle &handle) {
+            spdlog::get("console")->info("onRun:client->on:CloseEvent");
             ptr->close();
+        });
+
+
+        // On end event
+        client->on<uvw::EndEvent>([this](const uvw::EndEvent &, uvw::TcpHandle &handle) {
+
+            std::shared_ptr<std::string> uData=handle.data<std::string>();
+            std::string tag=*uData.get();
+
+            spdlog::get("console")->info("onRun:client->on:EndEvent");
+
+            // Remove all subscriptions
+            getSubscriptions()->removeAll(tag);
+
+            // Set the handle data to null
+            handle.data(nullptr);
+
+            // Close the handle
+            handle.close();
+        });
+
+        // On data event...
+        client->on<uvw::DataEvent>([this](const uvw::DataEvent &event, uvw::TcpHandle &handle) {
+
+
+
+            std::string sMessage(event.data.get(), event.length);
+
+            std::shared_ptr<std::string> uData=handle.data<std::string>();
+            std::string tag=*uData.get();
+
+            spdlog::get("console")->info("onRun:client->on:DataEvent -- Message: {0} {1} {2} -",handle.sock().ip,handle.sock().port,tag);
+
+            try {
+                // Try to parse the message
+                nlohmann::json jMessage = nlohmann::json::parse(sMessage);
+
+                // Manage the subscription
+                getSubscriptions()->parse(tag,jMessage,[&handle](nlohmann::json update) {
+                    std::string updateString = update.dump();
+
+                    //spdlog::get("console")->info("TcpIpSignalKDataServer/onUpdate: {0}", updateString);
+
+                    handle.tryWrite((char *)updateString.c_str(), static_cast<unsigned int>(updateString.length()));
+                });
+
+            } catch (nlohmann::detail::parse_error &exception) {
+                spdlog::get("console")->error("TcpIpSignalKDataServer: {0}",exception.what());
+            }
+
+
+
         });
 
         srv.accept(*client);
@@ -50,124 +111,58 @@ void TcpIpSignalKDataServer::onRun(){
         uvw::Addr local = srv.sock();
         uvw::Addr remote = client->peer();
 
-        std::string hello = pSignalKModel->getHello().dump();
+        // Set the id as user data
+        std::shared_ptr<std::string> uData=std::make_shared<std::string>(getId()+"|"+Utils::String::generate_hex(16));
 
-
-
-        //char *pHello=new char[hello.length()];
-        //strcpy(pHello,hello.c_str());
-        //auto dataTryWrite = std::unique_ptr<char[]>(pHello);
-        //client->tryWrite(std::move(dataTryWrite), (unsigned int)hello.length());
-        client->tryWrite((char *)hello.c_str(),hello.length());
-
-        //delete[] pHello;
-        //pHello=nullptr;
-
-
-
-        /*
-        std::shared_ptr<std::vector<nlohmann::json*>> uData=std::make_shared<std::vector<nlohmann::json*>>();
         client->data(uData);
-        std::vector<nlohmann::json*> *subscriptions=(std::vector<nlohmann::json*> *)uData.get();
-        nlohmann::json *subscription= new nlohmann::json(
-                {
-                        { "_tag", getId() },
-                        { "context", "*"},
-                        { "subscribe", { "*" }}
-                }
-        );
-        pSignalKModel->getUpdateBus()->subscribe(*subscription, [client](nlohmann::json update) {
-            spdlog::get("console")->debug("{0} - onUpdate", update.dump());
-            std::string updateString = update.dump();
-            //char *pUpdate=new char[updateString.length()];
-            //strcpy(pUpdate,updateString.c_str());
-            //auto dataTryWrite = std::unique_ptr<char[]>(pUpdate);
-            //client->tryWrite(std::move(dataTryWrite), (unsigned int)updateString.length());
-            client->tryWrite((char *)updateString.c_str(),updateString.length());
-        });
 
-        subscriptions->push_back(subscription);
-         */
-
-
-        client->on<uvw::DataEvent>([this](const uvw::DataEvent &event, uvw::TcpHandle &handle) {
-
-            std::string msg(event.data.get(), event.length);
-            spdlog::get("console")->info("{0} {1} - Message",handle.sock().ip,handle.sock().port);
-
-            std::string sMessage=std::string(event.data.get(), event.length);
-            try {
-                nlohmann::json jMessage = nlohmann::json::parse(sMessage);
-                if (jMessage["context"] && jMessage["subscribe"]) {
-                    jMessage["tag"]=getId();
-                    auto uData=handle.data();
-                    /*
-                    std::vector<nlohmann::json*> *subscriptions=(std::vector<nlohmann::json*> *)uData.get();
-                    nlohmann::json *subscription = new nlohmann::json(jMessage);
-
-                    pSignalKModel->getUpdateBus()->subscribe(*subscription, [&handle](nlohmann::json update) {
-                        spdlog::get("console")->debug("{0} - onUpdate", update.dump());
-                        std::string updateString = update.dump();
-                        char *pUpdate=new char[updateString.length()];
-                        strcpy(pUpdate,updateString.c_str());
-                        auto dataTryWrite = std::unique_ptr<char[]>(pUpdate);
-                        handle.tryWrite(std::move(dataTryWrite), (unsigned int)updateString.length());
-                        //delete[] pUpdate;
-                        pUpdate= nullptr;
-
-                    });
-
-                    subscriptions->push_back(subscription);
-                     */
-
-                }
-            } catch (nlohmann::detail::parse_error &exception) {
-                spdlog::get("console")->error("TcpIpSignalKDataServer: {0}",exception.what());
-            }
-
-        });
-
-        client->on<uvw::EndEvent>([this](const uvw::EndEvent &, uvw::TcpHandle &handle) {
-
-            auto uData=handle.data();
-            /*
-            std::vector<nlohmann::json*> *subscriptions=(std::vector<nlohmann::json*> *)uData.get();
-            std::for_each(subscriptions->begin(),subscriptions->end(),[this](nlohmann::json *subscription) {
-                pSignalKModel->getUpdateBus()->unsubscribe(*subscription);
-                delete subscription;
-            });
-            delete subscriptions;
-             */
-
-            handle.data(nullptr);
-
-
-            std::cout << "end" << std::endl;
-            int count = 0;
-            handle.loop().walk([&count](uvw::BaseHandle &) { ++count; });
-            std::cout << "still alive: " << count << " handles" << std::endl;
-            handle.close();
-        });
         client->read();
+        spdlog::get("console")->info("onRun:tcp->on:ListenEvent/END");
+
+
+
+
 
     });
 
-    tcp->once<uvw::CloseEvent>([](const uvw::CloseEvent &, uvw::TcpHandle &) {
-        std::cout << "close" << std::endl;
+
+    // On tcp close event
+    tcp->once<uvw::CloseEvent>([](const uvw::CloseEvent &, uvw::TcpHandle &handle) {
+        spdlog::get("console")->info("onRun:tcp->once:CloseEvent");
+        int count = 0;
+        handle.loop().walk([&count](uvw::BaseHandle &) { ++count; });
+        std::cout << "still alive: " << count << " handles" << std::endl;
+        handle.close();
     });
 
 
-    spdlog::get("console")->info("TcpIpSignalKDataServer/Listening: {0}:{1}",bind,port);
+    spdlog::get("console")->info("TcpIpSignalKDataServer/Bind: {0}:{1}",bind,port);
     tcp->bind(bind, port);
+    spdlog::get("console")->info("TcpIpSignalKDataServer/Listen: {0}:{1}",bind,port);
     tcp->listen();
+    spdlog::get("console")->info("onRun:tcp->once:ListenEvent/tHeartBeat-pre");
+    std::thread tHeartBeat([this,tcp](){
+        while(!isDone()) {
+            std::string hello = pSignalKModel->getHello().dump();
+            spdlog::get("console")->info("TcpIpSignalKDataServer/heartbeat: {0}",hello);
+            int count = 0;
+            tcp->loop().walk([&count,hello](uvw::BaseHandle &handle) {
+
+                //handle.tryWrite((char *)hello.c_str(),hello.length());
+
+
+                count++;
+            });
+            spdlog::get("console")->info("TcpIpSignalKDataServer/heartbeat: count {0}",count);
+
+            std::this_thread::sleep_for(std::chrono::milliseconds(millis));
+        }
+        tcp->close();
+    });
+    spdlog::get("console")->info("onRun:tcp->once:ListenEvent/tHeartBeat-post");
+    spdlog::get("console")->info("TcpIpSignalKDataServer/Run: {0}:{1}",bind,port);
     loop->run();
-    while(!isDone()) {
-        std::string hello = pSignalKModel->getHello().dump();
-        /*
-         * ToDo: send heartbeat to all connected clients
-         */
-        std::this_thread::sleep_for(std::chrono::milliseconds(millis));
-    }
+    spdlog::get("console")->info("TcpIpSignalKDataServer/Close: {0}:{1}",bind,port);
     loop->close();
     spdlog::get("console")->info("TcpIpSignalKDataServer/ENDED: {0}:{1}",bind,port);
 }
